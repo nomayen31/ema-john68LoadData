fe74103c8b13615656a222794804018567b2d562
nomayen <nomayen.ohin@gmail.com>
2023-01-27 06:41:09 +0600

    shop page size and limit data load

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -10,82 +10,108 @@ import './Shop.css';
 
 
 const Shop = () => {
-    const { products, count } = useLoaderData();
+    // const { products, count } = useLoaderData();
+    const [products, setProducts] = useState([]);
+    const [count, setCount] = useState([]);
     const [cart, setCart] = useState([]);
     const [page, setPage] = useState(0);
     const [size, setSize] = useState(10);
 
-    const pages = Math.ceil(count / size);
-
-    const clearCart = () => {
-        setCart([]);
-        deleteShoppingCart();
-    }
-
     useEffect(() => {
-        const storedCart = getStoredCart();
-        const savedCart = [];
-        for (const id in storedCart) {
-            const addedProduct = products.find(product => product._id === id);
-            if (addedProduct) {
-                const quantity = storedCart[id];
-                addedProduct.quantity = quantity;
-                savedCart.push(addedProduct);
-            }
-        }
-        setCart(savedCart);
-    }, [products])
+        const url = `http://localhost:5000/products?page=${page}&size=${size}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setCount(data.count);
+                setProducts(data.products);
+            })
 
-    const handleAddToCart = (selectedProduct) => {
-        console.log(selectedProduct);
-        let newCart = [];
-        const exists = cart.find(product => product._id === selectedProduct._id);
-        if (!exists) {
-            selectedProduct.quantity = 1;
-            newCart = [...cart, selectedProduct];
-        }
-        else {
-            const rest = cart.filter(product => product._id !== selectedProduct._id);
-            exists.quantity = exists.quantity + 1;
-            newCart = [...rest, exists];
-        }
 
-        setCart(newCart);
-        addToDb(selectedProduct._id);
+    }, [page, size])
+
+
+
+
+const pages = Math.ceil(count / size);
+
+const clearCart = () => {
+    setCart([]);
+    deleteShoppingCart();
+}
+
+useEffect(() => {
+    const storedCart = getStoredCart();
+    const savedCart = [];
+    for (const id in storedCart) {
+        const addedProduct = products.find(product => product._id === id);
+        if (addedProduct) {
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+        }
+    }
+    setCart(savedCart);
+}, [products])
+
+const handleAddToCart = (selectedProduct) => {
+    console.log(selectedProduct);
+    let newCart = [];
+    const exists = cart.find(product => product._id === selectedProduct._id);
+    if (!exists) {
+        selectedProduct.quantity = 1;
+        newCart = [...cart, selectedProduct];
+    }
+    else {
+        const rest = cart.filter(product => product._id !== selectedProduct._id);
+        exists.quantity = exists.quantity + 1;
+        newCart = [...rest, exists];
     }
 
-    return (
-        <div className='shop-container'>
-            <div className="products-container">
-                {
-                    products.map(product => <Product
-                        key={product._id}
-                        product={product}
-                        handleAddToCart={handleAddToCart}
-                    ></Product>)
-                }
-            </div>
-            <div className="cart-container">
-                <Cart clearCart={clearCart} cart={cart}>
-                    <Link to="/orders">
-                        <button>Review Order</button>
-                    </Link>
-                </Cart>
-            </div>
-            <div className='pagination'>
-                <p>currently selected page {page}</p>
-                {
-                    [...Array(pages).keys()].map(number => <button
-                        key={number}
-                        onClick={() =>setPage(number)}
-                    >
-                        {number}
-                    </button>)
-                }
-            </div>
+    setCart(newCart);
+    addToDb(selectedProduct._id);
+}
 
+return (
+    <div className='shop-container'>
+        <div className="products-container">
+            {
+                products.map(product => <Product
+                    key={product._id}
+                    product={product}
+                    handleAddToCart={handleAddToCart}
+                ></Product>)
+            }
         </div>
-    );
+        <div className="cart-container">
+            <Cart clearCart={clearCart} cart={cart}>
+                <Link to="/orders">
+                    <button>Review Order</button>
+                </Link>
+            </Cart>
+        </div>
+        <div className='pagination'>
+            <p>currently selected page {page} and size : {size}</p>
+            {
+                [...Array(pages).keys()].map(number => <button
+                    key={number}
+                    className={page === number && 'selected'}
+                    onClick={() => setPage(number)}
+                >
+                    {number}
+                </button>)
+            }
+
+            <select onChange={e => setSize(e.target.value)}>
+                <option value="5">5</option>
+                <option value="10" selected>10</option>
+                <option value="15">15</option>
+                <option value="20">20</option>
+
+            </select>
+        </div>
+
+    </div>
+);
 };
 
 export default Shop;
