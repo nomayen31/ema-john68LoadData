8547594d07b4a8e77f63a8ad0fdb63af3e8e7f09
nomayen <nomayen.ohin@gmail.com>
2023-01-30 20:42:46 +0600

    emajon complite

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -42,29 +42,29 @@ const clearCart = () => {
 useEffect(() => {
     const storedCart = getStoredCart();
     const savedCart = [];
-    const ids =Object.keys(storedCart);
+    const ids = Object.keys(storedCart); 
     console.log(ids);
-
-    fetch('',{
+    fetch('http://localhost:5000/productsByIds', {
         method: 'POST',
-        headers:{
+        headers: {
             'content-type': 'application/json'
         },
         body: JSON.stringify(ids)
     })
     .then(res => res.json())
-    .then(data =>{
+    .then(data => {
+        for (const id in storedCart) {
+        const addedProduct = data.find(product => product._id === id);
+        if (addedProduct) {
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+        }
+    }
+    setCart(savedCart);
 
     })
-    // for (const id in storedCart) {
-    //     const addedProduct = products.find(product => product._id === id);
-    //     if (addedProduct) {
-    //         const quantity = storedCart[id];
-    //         addedProduct.quantity = quantity;
-    //         savedCart.push(addedProduct);
-    //     }
-    // }
-    // setCart(savedCart);
+    
 }, [products])
 
 const handleAddToCart = (selectedProduct) => {
